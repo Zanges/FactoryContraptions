6dac3a5d49fd06b0b0cfa25e6dd0f2a55d4ca6b8
hnsdieteri <dasdieterlp@gmail.com>
2015-10-04 20:24:16 +0200

    Set standard Material for the Ores

--- src/main/java/net/zanges/factorycontraptions/block/BlockFactoryOre.java
+++ src/main/java/net/zanges/factorycontraptions/block/BlockFactoryOre.java
@@ -8,15 +8,13 @@ import net.minecraft.block.material.Material;
 public class BlockFactoryOre extends BlockFactoryContraptions {
 
     /**
-     *
-     * @param mat - Material for super {@link BlockFactoryContraptions}
      * @param resistance - the Ores resistance
      * @param hardness - the Ores hardness
      * @param level - the needed mininglevel of the Pickaxe
      */
-    public BlockFactoryOre(Material mat, float resistance, float hardness, int level)
+    public BlockFactoryOre(float resistance, float hardness, int level)
     {
-        super(mat);
+        super(Material.rock);
         this.setResistance(resistance);
         this.setHardness(hardness);
         this.setHarvestLevel("pickaxe", level);
--- src/main/java/net/zanges/factorycontraptions/block/BlockFactoryOreSpecial.java
+++ src/main/java/net/zanges/factorycontraptions/block/BlockFactoryOreSpecial.java
@@ -14,15 +14,14 @@ public class BlockFactoryOreSpecial extends BlockFactoryOre {
     Item drop;
 
     /**
-     * @param mat        - Material for super {@link BlockFactoryContraptions}
      * @param resistance - the Ores resistance
      * @param hardness   - the Ores hardness
      * @param level      - the needed mininglevel of the Pickaxe
      * @param drop       - the Item dropped by the Ore {@param null the Ore drops normal}
      * @param quantityRange - the maximum quantity + 1 dropped by the Ore
      */
-    public BlockFactoryOreSpecial(Material mat, float resistance, float hardness, int level, int quantityRange, Item drop) {
-        super(mat, resistance, hardness, level);
+    public BlockFactoryOreSpecial(float resistance, float hardness, int level, int quantityRange, Item drop) {
+        super(resistance, hardness, level);
         this.quantityRange = quantityRange;
         this.drop = drop;
     }
